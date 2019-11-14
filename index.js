56b7171e1e82f576ce91f93cfbdb56caa4082164
dy <df.creative@gmail.com>
2019-11-13 22:56:34 -0500

    Make work

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import { render, h, enhook } from './src/provider/auto.js'
+import * as lib from './src/provider/auto.js'
 import hooker from './src/enhook.js'
 
-export default enhook || function enhook(...args) {
-  if (!render || !h) {
-    if (!this || !this.render || !this.h) throw Error('Hooks provider is not found. `{ render, h }` must be provided as enhook.bind({ render, h }).')
-    return hooker.call(this)
-  }
+export default lib.enhook || function enhook(...args) {
+  let render = this && this.render || lib.render
+  let h = this && this.h || lib.h
+
+  if (!render || !h) throw Error('Hooks provider is not found. `{ render, h }` must be provided as `enhook.bind({ render, h })`')
+
   return hooker.call({ render, h }, ...args)
 }
 
